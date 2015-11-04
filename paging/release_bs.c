c9b0d49965858c221e28b8134a2354fe4d61abb8
mnsekh111 <mnsekh111@gmail.com>
2015-11-03 20:19:16 -0500

    Added bsm bsrelease

--- paging/release_bs.c
+++ paging/release_bs.c
@@ -8,16 +8,17 @@ SYSCALL release_bs(bsd_t bs_id) {
     /* release the backing store with ID bs_id */
     if (bs_id < 0 || bs_id > 15)
         return SYSERR;
+
     STATWORD ps;
     disable(ps);
-    struct bs_proc_map_t *temp;
-    temp = bsm_tab[bs_id].mapping;
-    if (temp == NULL) {
-        bsm_tab[bs_id].bs_status = BSM_UNMAPPED;
-        bsm_tab[bs_id].bs_npages = -1;
+
+    if (bsm_tab[bs_id].mapping == NULL) {
+        free_bsm(bs_id);
+        restore(ps);
+        return OK;
     }
     restore(ps);
-    return OK;
+    return SYSERR;
 
 }
 
